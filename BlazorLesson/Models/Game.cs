20b1be75f5dffa10941a1ef6308eb134156a40fb
Mark <sobchenkom6@gmail.com>
2022-12-16 04:51:53 +0200

    ChangedGameModel

--- BlazorLesson/Models/Game.cs
+++ BlazorLesson/Models/Game.cs
@@ -5,5 +5,6 @@
         public string? Name { get; set; }
         public string? Genre { get; set; }
         public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
     }
 }
